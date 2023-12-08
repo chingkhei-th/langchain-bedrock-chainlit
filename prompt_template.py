a421dab01a54f4628ee3a16fd24476155560cd97
Chingkheinganba Thoudam <72967745+chingkhei-th@users.noreply.github.com>
2023-12-08 20:15:11 +0530

    Edit prompt_template

--- prompt_template.py
+++ prompt_template.py
@@ -1,9 +1,9 @@
 def get_template(provider):
     templates = {
         "anthropic": """The following is a friendly conversation between a Human and an AI.
-
-The AI is a creative and can generates a details from its context, in its original language. 
-If the AI is asked questions that does not related to timeline creation, it truthfully says it can only help in creating timeline and cannot help with other questions.
+    The AI is a creative and can generates a details from its context, in its original language. 
+    If the AI is asked questions that does not related to timeline creation, it truthfully says it can only help in creating timeline and cannot help with other questions.
+    If Human ask about writting essays, writing emails, complex reasoning, or specialized skills like coding, it truthfully says it can only help in creating timeline and cannot help with other questions.    
 <conversation_history>
 {history}
 </conversation_history>
@@ -14,9 +14,3 @@ Assistant:"""
     }
     
     return templates.get(provider, "anthropic")
-
-"""
-The AI is talkative and provides lots of specific details from its context, in its original language. If the AI does not know the answer to a question, it truthfully says it does not know.
-Do not use any XML tags in the answer.
-"""
-    
